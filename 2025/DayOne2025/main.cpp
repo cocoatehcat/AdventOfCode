c0dffb32c4488fcf7c2bb3e2118d911353b17b9a
cocoatehcat <cocoa.teh.cat@gmail.com>
2025-12-25 16:00:24 -0500

    Formatted project and committing base

---
+++ 2025/DayOne2025/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by cocoa on 25/12/2025.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    return 0;
+}
