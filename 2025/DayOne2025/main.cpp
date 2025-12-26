0a44810a0a012426bc990edca5396c57fc6a93af
cocoatehcat <cocoa.teh.cat@gmail.com>
2025-12-25 20:07:50 -0500

    Figuring out the logic for rotation and where the dial should end up afterwards.

--- 2025/DayOne2025/main.cpp
+++ 2025/DayOne2025/main.cpp
@@ -5,10 +5,23 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
-
 int main() {
-    cout << "Hello World" << endl;
+
+    std::ifstream input;
+    input.open("input.txt");
+
+    int numTest = 0;
+    int testAmount = 5;
+    numTest -= testAmount;
+    if (numTest < 0) {
+        numTest = 100 + numTest;
+    }
+    else if (numTest > 100) {
+        numTest = 100 - numTest;
+    }
+    std::cout << numTest << std::endl;
+
+
 
     return 0;
 }
